3f3250ba7f4e2657c86f4790642ba2414ebf045f
Luan Victor <lvictor.dutra@gmail.com>
2025-06-13 20:50:56 -0300

    feat: implement createBuild function for build recommendations

---
+++ services/pcIdeal-api/modules/build.ts
@@ -0,0 +1,31 @@
+import { axiosRequest } from '@/services/utils/request';
+import { RecommendedBuilds } from '../models/recommendedBuild';
+
+export type CpuPreference = 'AMD' | 'INTEL' | 'no-preference';
+export type GpuPreference = 'AMD' | 'NVIDIA' | 'no-preference';
+export type UsageType = 'WORK' | 'GAMING' | 'CONTENT_CREATOR';
+
+type CreateBuildDataType = {
+  usageType: UsageType;
+  cpuPreference: CpuPreference;
+  gpuPreference: GpuPreference;
+  budget: number;
+};
+
+export async function createBuild({
+  usageType,
+  cpuPreference,
+  gpuPreference,
+  budget,
+}: CreateBuildDataType) {
+  return axiosRequest<RecommendedBuilds>({
+    method: 'POST',
+    url: '/api/builds/recommendations',
+    data: {
+      usage_type: usageType,
+      cpu_preference: cpuPreference,
+      gpu_preference: gpuPreference,
+      budget,
+    },
+  });
+}
