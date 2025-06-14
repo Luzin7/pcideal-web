5e03ff3c6418bba0a7102b97d41aa0dc8ec12161
Luan Victor <lvictor.dutra@gmail.com>
2025-06-14 18:11:58 -0300

    feat: add convertSmallestUnitToCurrency utility function for currency conversion

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const convertSmallestUnitToCurrency = (value: number) => {
+  return value / 100;
+};
